f77f949468dd4aaad4bf3722e472a02b359c7c44
Florianpog <Florian.pog@gmail.com>
2024-11-07 19:48:48 +0100

    ... working on it

--- Assets/IARVRAdaptor/IARVRCodeAdaptor.cs
+++ Assets/IARVRAdaptor/IARVRCodeAdaptor.cs
@@ -1,18 +1,28 @@
 using UnityEngine;
 using UnityEngine.XR;
 
-public class IARVRCodeAdaptor
+public class IARVRCodeAdaptor : MonoBehaviour
 {
-    public static float GetTriggerValue(GameObject controller)
+    public static IARVRCodeAdaptor instance;
+
+    public InputDevice RControler;
+    public InputDevice LControler;
+
+    public void Awake()
+    {
+        instance = this;
+    }
+
+    public static float GetTriggerValue(bool isRightController)
     {
 #if USING_OVR
-        OVRInput.Controller OVRcontroller = controller.GetComponent<OVRInput.Controller>;
+        OVRInput.Controller controller = isRightController? OVRInput.Controller.RHand :  OVRInput.Controller.LHand;
         
-        return OVRInput.Get(OVRInput.Axis1D.PrimaryHandTrigger, OVRcontroller);
+        return OVRInput.Get(OVRInput.Axis1D.PrimaryHandTrigger, controller);
 
 #else
         //using XR Toolkit verion
-        InputDevice device = InputDevices.GetDeviceAtXRNode(controller.GetComponent<XRNode>());
+        InputDevice device = (isRightController ? instance.RControler : instance.LControler);//InputDevices.GetDeviceAtXRNode((isRightController? instance.RControler : instance.LControler).gameobject.GetComponent<InputDevice>());
         if (device.isValid && device.TryGetFeatureValue(CommonUsages.trigger, out float triggerValue))
         {
             return triggerValue;
