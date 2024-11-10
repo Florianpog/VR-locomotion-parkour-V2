c95b85495c215700ee31b32daaf5b18ed0ac236f
Florianpog <Florian.pog@gmail.com>
2024-11-10 20:25:11 +0100

    Combined Ray selection for Force Interactrion + updated the colliders in the Scene

--- Assets/My/ForceInteraction.cs
+++ Assets/My/ForceInteraction.cs
@@ -1,3 +1,4 @@
+using System.Security.Cryptography;
 using UnityEngine;
 
 public class ForceInteraction : MonoBehaviour
@@ -7,26 +8,45 @@ public class ForceInteraction : MonoBehaviour
 
     public GameObject InteractionArea;
 
-    public float RayDistance;
-    public bool useEyeOferHand = true;
+    [Tooltip("0 beeing using HandDir, 1 being using the EyeOVerHand")]
+    public AnimationCurve HandRayTransition_vs_distance;
+    public float MaxRayDistance = 1000;
+    public bool useEyeOverHand = true;
+    public float InteractionAreaDistanceScaleFactor = 0.1f;
+
+    private float logBase = 10f;
 
     public void Update()
     {
-        Ray ray;
-        if (useEyeOferHand)
-        {
-            ray = new Ray(HandDirectionTransform.position, (HandDirectionTransform.position - XREyes.transform.position).normalized);
-        }
-        else
-        {
-            ray = new Ray(HandDirectionTransform.position, HandDirectionTransform.forward);
-        }
+        Vector3 eyeOverHand = (HandDirectionTransform.position - XREyes.transform.position).normalized;
+        Vector3 handDir = HandDirectionTransform.forward;
+        Vector3 averageDirection = (eyeOverHand + handDir).normalized;
+        Ray averageRay = new Ray(HandDirectionTransform.position, averageDirection);
+        RaycastHit averageRayHit;
+        float fullyTransitionedLastValue = HandRayTransition_vs_distance.keys[HandRayTransition_vs_distance.length - 1].time;
+        float fullyTransitionedDistance = Mathf.Pow(logBase, fullyTransitionedLastValue);
+        bool hasAverageRayHit = Physics.Raycast(averageRay, out averageRayHit, fullyTransitionedDistance);
+
+        float averageRayDistance = hasAverageRayHit? (averageRayHit.point - XREyes.transform.position).magnitude : fullyTransitionedDistance;
+        Debug.Log("averageRayDistance: " + averageRayDistance);
+        float logAverageRayDistance = Mathf.Log(averageRayDistance, logBase);
+        Debug.Log("logAverageRayDistance: " + logAverageRayDistance);
+        float combineValue = HandRayTransition_vs_distance.Evaluate(logAverageRayDistance);
+        Debug.Log("combineValue: " + combineValue);
+
+        Vector3 combinedDirection = (eyeOverHand * combineValue + handDir * (1 - combineValue)).normalized;
+        Ray combinedRay = new Ray(HandDirectionTransform.position, combinedDirection);
 
         RaycastHit hit;
-        bool hasHit = Physics.Raycast(ray, out hit, RayDistance);
+        bool hasHit = Physics.Raycast(combinedRay, out hit, MaxRayDistance);
         if (hasHit)
         {
             InteractionArea.transform.position = hit.point;
+
+            float distance = (hit.point - XREyes.transform.position).magnitude;
+            float scale = distance * InteractionAreaDistanceScaleFactor;
+            InteractionArea.transform.localScale = Vector3.one * scale;
         }
+
     }
 }
