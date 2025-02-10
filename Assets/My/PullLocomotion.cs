733175457b6471815a8728479d5ac318fda3c9d2
TimWeckbach <37379629+TimWeckbach@users.noreply.github.com>
2025-02-10 16:45:06 +0100

    locomotion only with trigger

--- Assets/My/PullLocomotion.cs
+++ Assets/My/PullLocomotion.cs
@@ -2,6 +2,7 @@ using System;
 using Unity.XR.CoreUtils;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.InputSystem;
 using UnityEngine.XR.Interaction.Toolkit.Locomotion;
 using System.Linq;
 
@@ -12,6 +13,7 @@ public class PullLocomotion : LocomotionProvider
     /// This Transform should be a child of the force interaction hand. The forward direction should be the way the hand is facting and the position the hand center
     /// </summary>
     public HandData<Transform> HandsHandForceInteractionTransform;
+    public HandData<InputActionReference> HandsPullAction;
     public Camera XREyes;
 
     public float baseMovementSpeed = 5.0f;
@@ -29,10 +31,20 @@ public class PullLocomotion : LocomotionProvider
     public Transform DebugObject; 
 
     private HandData<MyQueue<Tuple<Vector3, float>>> handsSavedLastHandLocalPos = new HandData<MyQueue<Tuple<Vector3, float>>>(new MyQueue<Tuple<Vector3, float>>(), new MyQueue<Tuple<Vector3, float>>()); //last pos is saved as local postion, because the player can move after the frame which will require transforming the pos to the new parent pos
-
+    private HandData<bool> handsIsPullingAction = new HandData<bool>(false, false);
 
     private CharacterController characterController;
     private bool attemptedGetCharacterController;
+
+    private void Start(){
+        
+        HandsPullAction.Left.action.started += (_) => StartPullAction(true);
+        HandsPullAction.Right.action.started += (_) => StartPullAction(false);
+
+        HandsPullAction.Left.action.canceled += (_) => StopPullAction(true);
+        HandsPullAction.Right.action.canceled += (_) => StopPullAction(false);
+
+    }
 
     private void Update()
     {
@@ -64,33 +76,37 @@ public class PullLocomotion : LocomotionProvider
 
             ref Transform handForceInteractionTransform = ref (handIsLeft ? ref HandsHandForceInteractionTransform.Left : ref HandsHandForceInteractionTransform.Right);
             ref MyQueue<Tuple<Vector3, float>> savedLastHandLocalPos = ref (handIsLeft ? ref handsSavedLastHandLocalPos.Left : ref handsSavedLastHandLocalPos.Right);
+            ref bool isPullingAction = ref (handIsLeft ? ref handsIsPullingAction.Left : ref handsIsPullingAction.Right);
 
-            Vector3 handPos = handForceInteractionTransform.position;
-            savedLastHandLocalPos.Enqueue(new Tuple<Vector3, float>(xrOrigin.transform.InverseTransformPoint(handPos), Time.deltaTime));
-            while(savedLastHandLocalPos.Count > 0 && savedLastHandLocalPos.Sum(t => t.Item2) > durationOfRelevantPastHandMovement)
-                savedLastHandLocalPos.Dequeue();
+            if(isPullingAction)
+            {
+                Vector3 handPos = handForceInteractionTransform.position;
+                savedLastHandLocalPos.Enqueue(new Tuple<Vector3, float>(xrOrigin.transform.InverseTransformPoint(handPos), Time.deltaTime));
+                while(savedLastHandLocalPos.Count > 0 && savedLastHandLocalPos.Sum(t => t.Item2) > durationOfRelevantPastHandMovement)
+                    savedLastHandLocalPos.Dequeue();
 
-            Vector3 stabalizedLocalHandVelocity = (savedLastHandLocalPos.Count > 0)? (savedLastHandLocalPos.Last().Item1 - savedLastHandLocalPos.First().Item1) / savedLastHandLocalPos.Sum(t => t.Item2) : Vector3.zero;
-            Vector3 stabalizedHandVelocity = xrOrigin.transform.TransformDirection(stabalizedLocalHandVelocity).normalized * stabalizedLocalHandVelocity.magnitude;
-            float factorFromHandSpeed = MovementFactor_vs_handSpeed.Evaluate(stabalizedHandVelocity.magnitude);
+                Vector3 stabalizedLocalHandVelocity = (savedLastHandLocalPos.Count > 0)? (savedLastHandLocalPos.Last().Item1 - savedLastHandLocalPos.First().Item1) / savedLastHandLocalPos.Sum(t => t.Item2) : Vector3.zero;
+                Vector3 stabalizedHandVelocity = xrOrigin.transform.TransformDirection(stabalizedLocalHandVelocity).normalized * stabalizedLocalHandVelocity.magnitude;
+                float factorFromHandSpeed = MovementFactor_vs_handSpeed.Evaluate(stabalizedHandVelocity.magnitude);
 
-            //if (!handIsLeft)
-            //    DebugObject.localPosition = new Vector3(DebugObject.localPosition.x, stabalizedHandVelocity.magnitude, DebugObject.localPosition.z);
+                //if (!handIsLeft)
+                //    DebugObject.localPosition = new Vector3(DebugObject.localPosition.x, stabalizedHandVelocity.magnitude, DebugObject.localPosition.z);
 
-            float handMovementAlignmentAngle = Vector3.Angle(-eyesDir, stabalizedHandVelocity);
-            float factorFromAngle = MovementFactor_vs_alignmentAngle.Evaluate(handMovementAlignmentAngle);
+                float handMovementAlignmentAngle = Vector3.Angle(-eyesDir, stabalizedHandVelocity);
+                float factorFromAngle = MovementFactor_vs_alignmentAngle.Evaluate(handMovementAlignmentAngle);
 
-            if (!handIsLeft)
-                DebugObject.localPosition = new Vector3(DebugObject.localPosition.x, handMovementAlignmentAngle / 90f, DebugObject.localPosition.z);
+                if (!handIsLeft)
+                    DebugObject.localPosition = new Vector3(DebugObject.localPosition.x, handMovementAlignmentAngle / 90f, DebugObject.localPosition.z);
 
-            Vector3 rawMovmentDir = eyesDir;
-            Vector3 groundAngle = (new Vector3(rawMovmentDir.x, 0, rawMovmentDir.z).normalized); //!!!should be improved by raycast ground checks
-            float groundAlignmentAngle = Vector3.Angle(rawMovmentDir, groundAngle);
-            Vector3 movmentDir = Vector3.Lerp(rawMovmentDir, groundAngle, groundOverride_vs_groundAngle.Evaluate(groundAlignmentAngle));
+                Vector3 rawMovmentDir = eyesDir;
+                Vector3 groundAngle = (new Vector3(rawMovmentDir.x, 0, rawMovmentDir.z).normalized); //!!!should be improved by raycast ground checks
+                float groundAlignmentAngle = Vector3.Angle(rawMovmentDir, groundAngle);
+                Vector3 movmentDir = Vector3.Lerp(rawMovmentDir, groundAngle, groundOverride_vs_groundAngle.Evaluate(groundAlignmentAngle));
 
-            Vector3 newMovement = movmentDir * (baseMovementSpeed * factorFromAngle * factorFromHandSpeed);
+                Vector3 newMovement = movmentDir * (baseMovementSpeed * factorFromAngle * factorFromHandSpeed);
 
-            movement += newMovement;
+                movement += newMovement;
+            }
         }
 
         Vector3 motion = movement + verticalVelocity;
@@ -99,6 +115,21 @@ public class PullLocomotion : LocomotionProvider
 
         TryEndLocomotion();
     }
+
+    
+    private void StartPullAction(bool handIsLeft){
+        ref InputActionReference pullAction = ref (handIsLeft ? ref HandsPullAction.Left : ref HandsPullAction.Right);
+        ref bool isPullingAction = ref (handIsLeft ? ref handsIsPullingAction.Left : ref handsIsPullingAction.Right);
+        isPullingAction = true;
+
+    }
+
+    private void StopPullAction(bool handIsLeft){
+        ref InputActionReference pullAction = ref (handIsLeft ? ref HandsPullAction.Left : ref HandsPullAction.Right);
+        ref bool isPullingAction = ref (handIsLeft ? ref handsIsPullingAction.Left : ref handsIsPullingAction.Right);
+        isPullingAction = false;
+    }
+
 
     /*private Vector3 CalculateMovement()
     {
